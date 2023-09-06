22689b8442d5c26b043b13ec93fa1b149ac296c7
DonovanReynolds <donovanreynolds123@gmail.com>
2023-09-06 19:21:41 -0400

    Small starting point
    Start to the website with small introductions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import pigeon from "./BirdBackground.jpg"
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src = {pigeon} className='Pigeon-picture'></img>
+
+        <div>
+          <h1 className='Page-title'>
+            Pigeon Legion
+          </h1>
+          <p className='Page-intro'>
+            Intro will go here!
+          </p>
+        </div>
+        <div>
+        <div className='people-container'>
+            <div>Computer Engineering</div>
+            <div>Electrical Engineering</div>
+            <div>Donovan Reynolds: do106772@ucf.edu</div>
+            <div>Rodrigo Guerra: TBA@ucf.edu</div>
+            <div></div><div></div>
+            <div>Photonics</div>
+            <div>Photonics</div>
+            <div>Aiden Nipper: TBA@ucf.edu</div>
+            <div>Will DiSalvo: TBA@ucf.edu</div>
+          </div>
+        </div>
       </header>
     </div>
+
   );
 }
 
